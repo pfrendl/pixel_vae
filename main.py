97e6c4a4ed5243b76bdf26a16c68b7dfe5c34877
Peter Frendl <pfre00@gmail.com>
2021-01-23 15:46:35 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+import glob
+from PIL import Image
+import numpy as np
+import torch
+import cv2
+from skimage.transform import resize
+
+import models
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda")
+    img_embedding_size = 128
+    pos_embedding_size = 32
+    decoder_hidden_size = 128
+    batch_size = 1
+
+    image_paths = glob.glob("images/*.jfif")
+    images = [np.asarray(Image.open(image_path)) for image_path in image_paths]
+    for i in range(len(images)):
+        image = images[i]
+        dimensions = image.shape[:2]
+        ratio = min([min(d, 480) / d for d in dimensions])
+        scaled_dimensions = [int(ratio * d) for d in dimensions]
+        image = resize(image, (*scaled_dimensions, 3), anti_aliasing=True)
+        image = image[:, :, [2, 1, 0]]
+        image = np.transpose(image, (2, 0, 1))
+        images[i] = image
+
+    encoder = models.Encoder(
+        embedding_size=img_embedding_size
+    ).to(device)
+    decoder = models.Decoder(
+        img_embedding_size=img_embedding_size,
+        pos_embedding_size=pos_embedding_size,
+        hidden_size=decoder_hidden_size
+    ).to(device)
+
+    encoder_opt = torch.optim.Adam(encoder.parameters(), lr=0.001)
+    decoder_opt = torch.optim.Adam(decoder.parameters(), lr=0.001)
+    encoder_scheduler = torch.optim.lr_scheduler.ExponentialLR(encoder_opt, gamma=0.99999)
+    decoder_scheduler = torch.optim.lr_scheduler.ExponentialLR(decoder_opt, gamma=0.99999)
+
+    for i in range(100000000):
+        image_idxs = np.random.randint(low=0, high=len(images), size=batch_size)
+        image = [images[idx] for idx in image_idxs]
+        shape_idx = np.random.randint(low=0, high=batch_size)
+        image_shape = image[shape_idx].shape
+        if batch_size > 1:
+            image = [resize(img, image_shape, anti_aliasing=True) for img in image]
+        image = np.stack(image, axis=0)
+        image = torch.tensor(image, dtype=torch.float32, device=device)
+
+        mean, log_std = encoder(image)
+        std = log_std.exp()
+
+        sample_dist = torch.distributions.Normal(loc=mean, scale=std)
+        sample = sample_dist.sample()
+
+        height, width = image_shape[1:3]
+        # TODO sample instead of mean
+        decoded_image = decoder(mean, height, width)
+
+        kl_loss = 0.5 * (mean ** 2 + std ** 2 - 2 * log_std - 1).sum(dim=1)
+        kl_loss = kl_loss.mean()
+        l2_loss = ((image - decoded_image) ** 2).sum(dim=1).mean(dim=(1, 2))
+        l2_loss = l2_loss.mean()
+        # TODO use kl_loss
+        loss = 0 * kl_loss + l2_loss
+        print(kl_loss.item(), l2_loss.item(), encoder_opt.state_dict()["param_groups"][0]["lr"])
+
+        encoder_opt.zero_grad()
+        decoder_opt.zero_grad()
+        loss.backward()
+        encoder_opt.step()
+        decoder_opt.step()
+        encoder_scheduler.step()
+        decoder_scheduler.step()
+
+        if i % 40 == 0:
+            np_image = decoded_image[shape_idx].permute((1, 2, 0)).detach().cpu().numpy()
+            # np_image = image[0].permute((1, 2, 0)).detach().cpu().numpy()
+            cv2.imshow('image', np_image)
+            cv2.waitKey(1)
---
+++ models.py
@@ -0,0 +1,104 @@
+from typing import Tuple
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+
+def create_pixel_coords(height: int, width: int, device=torch.device):
+    dim0_range = torch.linspace(start=-2, end=2, steps=height).to(device)
+    dim1_range = torch.linspace(start=-2, end=2, steps=width).to(device)
+    grids = torch.meshgrid(dim0_range, dim1_range)
+    pixel_coords = torch.stack(grids, dim=-1)
+    return pixel_coords
+
+
+class Encoder(nn.Module):
+    def __init__(self, embedding_size: int):
+        super(Encoder, self).__init__()
+        self.cl0 = nn.Conv2d(3, 8, 3, 2, 2)
+        self.cl1 = nn.Conv2d(8, 16, 3, 2, 2)
+        self.cl2 = nn.Conv2d(16, 32, 3, 2, 2)
+        self.cl3 = nn.Conv2d(32, 32, 3, 2, 2)
+        self.cl4 = nn.Conv2d(32, 32, 3, 2, 2)
+        self.cl5 = nn.Conv2d(32, 64, 3, 2, 2)
+        self.cl6 = nn.Conv2d(64, embedding_size, 3, 2, 2)
+        self.fc0 = nn.Linear(embedding_size, embedding_size)
+        self.mean = nn.Linear(embedding_size, embedding_size)
+        self.std = nn.Linear(embedding_size, embedding_size)
+
+    def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
+        x = torch.relu(self.cl0(x))
+        x = torch.relu(self.cl1(x))
+        x = torch.relu(self.cl2(x))
+        x = torch.relu(self.cl3(x))
+        x = torch.relu(self.cl4(x))
+        x = torch.relu(self.cl5(x))
+        x = torch.relu(self.cl6(x))
+        # print("x.shape ---", x.shape)
+        x = torch.mean(x, dim=(2, 3))
+        x = torch.relu(self.fc0(x))
+        mean = self.mean(x)
+        log_std = self.std(x)
+        return mean, log_std
+
+
+class SkipBlock(nn.Module):
+    def __init__(self, size: int, scaling: float):
+        super(SkipBlock, self).__init__()
+        scaled_size = int(scaling * size)
+        self.fc0 = nn.Linear(size, scaled_size)
+        self.fc1 = nn.Linear(scaled_size, scaled_size)
+        self.fc2 = nn.Linear(scaled_size, size)
+
+    def forward(self, x):
+        y = torch.relu(self.fc0(x))
+        y = torch.relu(self.fc1(y))
+        y = self.fc2(y)
+        return x + y
+
+
+class PositionEmbedding(nn.Module):
+    def __init__(self, embedding_size: int):
+        super(PositionEmbedding, self).__init__()
+        self.fc0 = nn.Linear(2, 128)
+        self.fc1 = nn.Linear(128, embedding_size)
+
+    def forward(self, height: int, width: int, device: torch.device) -> Tensor:
+        dim0_range = torch.linspace(start=-2, end=2, steps=height).to(device)
+        dim1_range = torch.linspace(start=-2, end=2, steps=width).to(device)
+        grids = torch.meshgrid(dim0_range, dim1_range)
+        pixel_coords = torch.stack(grids, dim=-1)  # (height, width, 2)
+        x = pixel_coords.view((-1, 2))  # (height * width, 2)
+        x = torch.relu(self.fc0(x))  # (height * width, embedding_size)
+        x = self.fc1(x)  # (height * width, embedding_size)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, img_embedding_size: int, pos_embedding_size: int, hidden_size: int):
+        super(Decoder, self).__init__()
+        self.position_embedding = PositionEmbedding(pos_embedding_size)
+        self.fc0 = nn.Linear(img_embedding_size + pos_embedding_size, hidden_size)
+        self.sb0 = SkipBlock(hidden_size, 1)
+        self.sb1 = SkipBlock(hidden_size, 1)
+        self.fc1 = nn.Linear(hidden_size, 3)
+
+    def forward(self, embedding: Tensor, height: int, width: int) -> Tensor:
+        x = embedding  # (batch_size, embedding_size)
+
+        position_embeddings = self.position_embedding(height, width, embedding.device)  # (height * width, 2)
+        position_embeddings = position_embeddings[None, :, :].expand(x.shape[0], -1, -1)  # (batch_size, height * width, 2)
+        x = x[:, None, :].expand(-1, position_embeddings.shape[1], -1)  # (batch_size, height * width, embedding_size)
+
+        x = torch.cat([x, position_embeddings], axis=2)  # (batch_size, height * width, embedding_size + 2)
+        x = x.view((-1, x.shape[2]))  # (batch_size * height * width, embedding_size + 2)
+
+        x = torch.relu(self.fc0(x))  # (batch_size * height * width, embedding_size)
+        x = self.sb0(x)
+        x = self.sb1(x)
+        x = self.fc1(x)  # (batch_size * height * width, 3)
+
+        x = x.view((embedding.shape[0], height, width, 3))  # (batch_size, height, width, 3)
+        x = x.permute((0, 3, 1, 2))  # (batch_size, 3, height, width)
+
+        return x
